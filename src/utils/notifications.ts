b0dc7bf7c9a15819589620022a49f266676b79be
pvdeval <pariksheet@gmail.com>
2026-06-09 11:52:27 +0530

    Disable notifications test

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -10,11 +10,5 @@ export function sendNotification(
   title: string,
   body: string,
 ) {
-  if (
-    Notification.permission === 'granted'
-  ) {
-    new Notification(title, {
-      body,
-    });
-  }
+  console.log(title, body);
 }
