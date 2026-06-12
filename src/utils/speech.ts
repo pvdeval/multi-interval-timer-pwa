0452893eed764e0729b12e8bcaca08806673c74a
pvdeval <pariksheet@gmail.com>
2026-06-12 11:41:31 +0530

    Revert  beep sound

--- src/utils/speech.ts
+++ src/utils/speech.ts
@@ -1,52 +1,23 @@
 export function speak(
   text: string,
 ) {
-  try {
-    if (
-      !(
-        'speechSynthesis' in
-        window
-      )
-    ) {
-      console.log(
-        'Speech synthesis not supported',
-      );
-      return;
-    }
-
-    speechSynthesis.cancel();
-
-    const utterance =
-      new SpeechSynthesisUtterance(
-        text,
-      );
-
-    utterance.rate = 1;
-    utterance.pitch = 1;
-    utterance.volume = 1;
-
-    utterance.onstart =
-      () => {
-        console.log(
-          'Speech started:',
-          text,
-        );
-      };
-
-    utterance.onerror =
-      (event) => {
-        console.log(
-          'Speech error:',
-          event,
-        );
-      };
-
-    speechSynthesis.speak(
-      utterance,
-    );
-  } catch (error) {
-    console.error(
-      error,
-    );
+  if (
+    !(
+      'speechSynthesis' in
+      window
+    )
+  ) {
+    return;
   }
+
+  const utterance =
+    new SpeechSynthesisUtterance(
+      text,
+    );
+
+  utterance.rate = 1;
+
+  speechSynthesis.speak(
+    utterance,
+  );
 }
